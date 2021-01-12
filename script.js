bb212f68cc6537605e43700c3611a105556a0ba5
alyson farias <ramos.alysonfarias@gmail.com>
2021-01-11 21:30:29 -0300

    criar cobrinha

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-let canvas = document.getElementById("snakee");
+let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+
+snake[0] = {
+    x: 7*box,
+    y: 7*box
+}
 
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0,0, 16 * box , 16 * box);
 }
+
+function criarCobra(){
+    for (let index = 0; index < snake.length; index++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[index].x, snake[index].y,box, box);
+        
+    }
+}
+
 criarBG();
+criarCobra();
