be330df68059b109dea678249d08b6a87db4ef9d
alyson farias <ramos.alysonfarias@gmail.com>
2021-01-11 21:18:50 -0300

    criando bg

---
+++ script.js
@@ -0,0 +1,9 @@
+let canvas = document.getElementById("snakee");
+let context = canvas.getContext("2d");
+let box = 32;
+
+function criarBG(){
+    context.fillStyle = "lightgreen";
+    context.fillRect(0,0, 16 * box , 16 * box);
+}
+criarBG();
