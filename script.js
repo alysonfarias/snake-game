dab8240c9a2207cdf4e61db4a86812b0d084e40a
alyson farias <ramos.alysonfarias@gmail.com>
2021-01-11 21:55:59 -0300

    movimento snake

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
-
+let direction = "right";
 snake[0] = {
     x: 7*box,
     y: 7*box
 }
+
 
 function criarBG(){
     context.fillStyle = "lightgreen";
@@ -20,6 +21,30 @@ function criarCobra(){
         
     }
 }
+function iniciarJogo(){
+    criarBG();
+    criarCobra();
+    
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
 
-criarBG();
-criarCobra();
+    if(direction ==  "right") snakeX += box;
+    if(direction ==  "left") snakeX -= box;
+    if(direction ==  "up") snakeX -= box;
+    if(direction ==  "down") snakeX += box;
+
+    snake.pop();
+    
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+     snake.unshift(newHead);
+
+
+
+}
+iniciarJogo();
+
+
+let jogo = setInterval(iniciarJogo, 100);
